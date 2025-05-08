1681a06dbf30f5d4609bf6318a85766a702884d4
عمار هيثم عبدالله قباني <39908704+ammarkabbany@users.noreply.github.com>
2025-05-09 02:28:04 +0300

    Refactor code structure for improved readability and maintainability Introduce better look

--- src/components/TeethFormData.tsx
+++ src/components/TeethFormData.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Material } from "@/types";
 import React, { useState } from "react";
 import { Label } from "@/components/ui/label";
@@ -39,7 +39,7 @@ export default function TeethFormData({
 
   const doHandleSelectToothMaterial = (value: string) => {
     const choice = materials.find(
-      (material: Material) => material.$id === value,
+      (material: Material) => material.$id === value
     );
     handleChangeToothMaterial({ ...selectedTooth, material: choice });
     setSelectedTooth((prevTooth) => {
@@ -100,12 +100,26 @@ export default function TeethFormData({
 
   return (
     <div className="flex flex-col items-center space-y-4 lg:space-y-6">
+      {/* <svg className="bg-white"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="200 0 100 500"
+        width="500"
+        height="500"
+        preserveAspectRatio="xMinYMin meet"
+        style={{ maxWidth: "100%", height: "auto" }}>
+        <g xmlns="http://www.w3.org/2000/svg">
+          <path
+            fill="#373737"
+            d="M 603.5,172.5 C 613.056,171.758 622.056,173.592 630.5,178C 637.137,183.711 639.304,190.877 637,199.5C 634.564,214.478 629.564,228.478 622,241.5C 611.389,249.645 600.889,249.478 590.5,241C 576.097,228.363 568.597,212.53 568,193.5C 573.44,181.251 582.94,174.584 596.5,173.5C 598.415,173.216 600.081,173.549 601.5,174.5C 590.384,174.812 580.884,178.812 573,186.5C 571.441,188.217 570.608,190.217 570.5,192.5C 569.484,211 576.15,226.5 590.5,239C 599.483,246.409 609.15,247.409 619.5,242C 624.663,234.862 628.496,227.029 631,218.5C 633.408,209.958 635.242,201.291 636.5,192.5C 636.547,187.945 634.881,184.278 631.5,181.5C 626.221,176.626 619.888,174.292 612.5,174.5C 609.5,173.833 606.5,173.167 603.5,172.5 Z"
+          />
+        </g>
+      </svg> */}
       <ToothCheckbox
-      // onChange={(v) => handleCheckTeeth(e, v)}
-      onChange={(e, v) => handleCheckTeeth(e, v)}
-      teethData={checkedTeeth}
-      onReset={resetTeeth}
-      showLabels
+        // onChange={(v) => handleCheckTeeth(e, v)}
+        onChange={(e, v) => handleCheckTeeth(e, v)}
+        teethData={checkedTeeth}
+        onReset={resetTeeth}
+        showLabels
       />
       {/* <div className="relative max-w-[125px] mx-auto">
         <div className="flex flex-col gap-1">
@@ -175,7 +189,9 @@ export default function TeethFormData({
         </div>
       </div> */}
       <div className="text-center text-sm text-muted-foreground mt-4">
-        <p>Select teeth by clicking. Use Shift+Click to select multiple teeth.</p>
+        <p>
+          Select teeth by clicking. Use Shift+Click to select multiple teeth.
+        </p>
       </div>
 
       {/* Controls */}
@@ -190,7 +206,8 @@ export default function TeethFormData({
               <SelectValue placeholder="Select a tooth" />
             </SelectTrigger>
             <SelectContent>
-              {data?.sort((a, b) => a?.label - b?.label)
+              {data
+                ?.sort((a, b) => a?.label - b?.label)
                 .map((t, i) => (
                   <SelectItem key={i} value={String(t.label)}>
                     Tooth {t.label}
