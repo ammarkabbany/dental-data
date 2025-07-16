d994be49e2d4e86a66073c6219d0271123bd78df
عمار هيثم عبدالله قباني <39908704+ammarkabbany@users.noreply.github.com>
2025-07-17 01:51:53 +0300

    Enhance execution logs and dashboard components - Updated `listAllExecutions` to include `functionName` in the returned execution logs for better context. - Modified `useGetAdminExecutions` to ensure consistent refetching behavior by setting `refetchOnMount`, `refetchOnReconnect`, and `refetchOnWindowFocus` to "always". - Changed the description in the dashboard card for managing lab materials to better reflect its purpose. - Refactored `ExecutionLogs` component to improve readability and added a link to the function details in the logs. - Adjusted the layout in `PrintComponent` for better alignment of total units display. - Updated doctor ID display format in `columns.tsx` to prepend "DOC-" for clarity. - Introduced `APPWRITE_PROJECT` constant in `constants.ts` for better configuration management.

--- src/app/(admin)/monitor/page.tsx
+++ src/app/(admin)/monitor/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import { ExecutionLogs } from "@/components/admin/monitoring/ExecutionLogs";
 import { useGetAdminExecutions } from "./use-get-executions";
+import { Models } from "node-appwrite";
 
 export default function Page() {
   const {data: executions} = useGetAdminExecutions();
 
   return (
     <div>
-      <ExecutionLogs logs={executions || []} />
+      <ExecutionLogs logs={executions as (Models.Execution & { functionName: string })[]} />
     </div>
   )
 }
